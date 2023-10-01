0f61379590c0f327e46e11ae7db6f31d2d22f9e3
Mathias Raa <skraagen@hotmail.com>
2023-10-01 15:21:43 +0200

    data load functions

--- src/utils/data_load.py
+++ src/utils/data_load.py
@@ -1,38 +1,41 @@
-# %%
 import pandas as pd
 import numpy as np
 import os
 
-
-# %%
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_path = dir_path + "/../../data/Data"
 data = []
 
-# for root, dirs, files in os.walk(data_path, topdown=False):
-#     for name in dirs:
-#         print(name)
 
-print([f.name for f in os.scandir(data_path) if f.is_dir()])
-
-# %%
+def get_user_ids():
+    return [f.name for f in os.scandir(data_path) if f.is_dir()]
 
 
-for f in os.scandir(data_path):
-    if f.is_dir():
-        for root, dirs, files in os.walk(f"{f.path}/Trajectory"):
-            trajectories = []
-            for name in files[0]:
-                trajectories.append(
-                    np.genfromtxt(
-                        f"{f.path}/Trajectory/{name}", delimiter=",", skip_header=6
-                    )
-                )
-            data.append(trajectories)
+def get_trajectories_df(user_id):
+    for _, _, files in os.walk(f"{data_path}/{user_id}/Trajectory"):
+        trajectories = pd.DataFrame()
+        for name in files:
+            trajectory = pd.read_csv(
+                f"{data_path}/{user_id}/Trajectory/{name}",
+                names=[
+                    "latitude",
+                    "longitude",
+                    ".",
+                    "altitude",
+                    "days",
+                    "date",
+                    "time",
+                ],
+                sep=",",
+                encoding="ISO-8859-1",
+                skiprows=6,
+            )
+            trajectory["id"] = name.split(".")[0]
 
-print(data[0])
+            # Add trajectory to trajectories dataframe
+            trajectories = pd.concat([trajectories, trajectory])
 
-# %%
+        return trajectories
 
 
 def load_data():
