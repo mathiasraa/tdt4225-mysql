b4488ff2b0721422a345b521520c6ed08859f295
Mathias Raa <skraagen@hotmail.com>
2023-10-01 16:12:23 +0200

    additional functions

--- src/utils/data_load.py
+++ src/utils/data_load.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from numpy import loadtxt
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -11,10 +12,41 @@ def get_user_ids():
     return [f.name for f in os.scandir(data_path) if f.is_dir()]
 
 
-def get_trajectories_df(user_id):
+def get_labeled_ids():
+    lines = loadtxt(f"{dir_path}/../../data/labeled_ids.txt", dtype=str)
+
+    return lines
+
+
+def get_activities_df(user_id):
+    for _, _, files in os.walk(f"{data_path}/{user_id}/Trajectory"):
+        activities = pd.DataFrame(
+            columns=["id"], data={"id": [name.split(".")[0] for name in files]}
+        )
+        activities["user_id"] = user_id
+
+        if user_id in get_labeled_ids():
+            activities[
+                ["transportation_mode", "start_date_time", "end_date_time"]
+            ] = loadtxt(
+                f"{data_path}/{user_id}/labels.txt",
+                skiprows=1,
+                delimiter="\t",
+                dtype=str,
+            )
+        else:
+            activities["transportation_mode"] = float("nan")
+            activities["start_date_time"] = float("nan")
+            activities["end_date_time"] = float("nan")
+
+        return activities
+
+
+def get_trajectories_df(user_id, activity_id):
     for _, _, files in os.walk(f"{data_path}/{user_id}/Trajectory"):
         trajectories = pd.DataFrame()
         for name in files:
+            # if activity_id == name.split(".")[0]:
             trajectory = pd.read_csv(
                 f"{data_path}/{user_id}/Trajectory/{name}",
                 names=[
@@ -30,7 +62,7 @@ def get_trajectories_df(user_id):
                 encoding="ISO-8859-1",
                 skiprows=6,
             )
-            trajectory["id"] = name.split(".")[0]
+            trajectory["activity_id"] = name.split(".")[0]
 
             # Add trajectory to trajectories dataframe
             trajectories = pd.concat([trajectories, trajectory])
