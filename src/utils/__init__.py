fa9ba2ca2d13c9cb1acec05418de696bd69308f8
Mathias Raa <skraagen@hotmail.com>
2023-10-04 00:28:10 +0200

    queries Co-authored-by: marcusfuglestad <marcusfuglestad@users.noreply.github.com>

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,10 +7,13 @@ from .data import (
 
 from .connection import MySQLConnector
 
+from .haversine_np import haversine_np
+
 __all__ = [
     "get_trajectories_df",
     "get_user_ids",
     "get_activities_df",
     "get_labeled_ids",
     "MySQLConnector",
+    "haversine_np",
 ]
